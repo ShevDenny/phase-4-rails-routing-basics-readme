7a3e0d94810a2ba1d03d2278cac9b05cba27d9b0
ShevDenny <sdenny904@gmail.com>
2021-07-18 15:53:15 -0400

    commit tree 58da6cdd93ebe2266e5d32e3d8fbc0af3e95f3e1

---
+++ app/controllers/cheeses_controller.rb
@@ -0,0 +1,9 @@
+class CheesesController < ApplicationController
+
+    def index
+        # model
+        cheeses = Cheese.order(price: :desc)
+        # view
+        render json: cheeses
+    end
+end
